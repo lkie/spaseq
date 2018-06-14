bdd140704521320f7c4d796fb3fcc048ac839cbc
lkie <l.kielpinski@gmail.com>
2018-06-14 11:30:38 +0200

    makeRandSeq multiple seqences

--- R/makeRandSeq.R
+++ R/makeRandSeq.R
@@ -1,7 +1,5 @@
 #' Make random sequence
 #'
-#' Function plots a heatmap of sequence preferences 
-#' and optionally a barplot with information content
 #'
 #' @param length length of the sequence
 #' @param nts nucleotides to use
@@ -10,6 +8,9 @@
 #' @return Character with a randomized sequence
 #' @export makeRandSeq
 
-makeRandSeq <- function(length, nts = c("A", "C", "G", "T"), prob = rep(0.25, 4)){
-  paste(sample(x = nts, size = length, prob = prob, replace = T), collapse = "")
+makeRandSeq <- function(length, nts = c("A", "C", "G", "T"), prob = rep(0.25, 4), n = 1){
+  generated_sequences <- rep(NA, i)
+  for(i in 1:n){
+    generated_sequences[i] <- paste(sample(x = nts, size = length, prob = prob, replace = T), collapse = "")
+    }
 }
